b02d41da265d43900d37be0005188c167ebe317e
sparsh-solvetude <sparsh-solvetude>
2025-07-08 22:47:29 +0530

    ts fix

--- src/app/signatureProjectPage/MapView/index.tsx
+++ src/app/signatureProjectPage/MapView/index.tsx
@@ -5,7 +5,6 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Fix Leaflet icon paths
-// @ts-expect-error: _getIconUrl is a private property not typed in leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
